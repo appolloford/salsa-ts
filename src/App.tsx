645879af570ec0c3d302f7a213fb8386be9a436d
Chia-Jung Hsu <appolloford@gmail.com>
2021-09-24 17:29:08 +0200

    Add plotly and blueprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+import { FileInput } from '@blueprintjs/core';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // const { text, buttonText } = {"Choose File...", "Uplaod"}
+  const text = "Choose File..."
+  const buttonText = "Upload"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FileInput />
+      {/* <input type="file" id="actual-btn" hidden/> */}
+      {/* <form>
+        <input type="file" id="myFile" name="filename" />
+        <input type="submit" />
+      </form> */}
+      <div>
+        <Plot
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            },
+            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          ]}
+          layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+        />
+      </div>
     </div>
   );
 }
