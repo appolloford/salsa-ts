3f6711ea02442e03acac6986698040bf962bc08a
grimmer <grimmer0125@gmail.com>
2021-10-04 23:00:15 +0800

    Fetch fitsreader.py initially, use d4c-queue to enqueue and use yarn instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useRef } from 'react';
-import Plot from 'react-plotly.js';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { FileInput } from '@blueprintjs/core';
 import Viewer from './components/Viewer';
+import { D4C } from "d4c-queue";
 import script from './python/fitsreader.py';
 import './App.css';
 
@@ -14,14 +14,16 @@ declare global { // <- [reference](https://stackoverflow.com/a/56458070/11542903
   }
 }
 
-const pyodideInstance = async () => {
+const d4c = new D4C();
+const pyodideInstance = d4c.wrap(async () => {
   const pyodide = await window.loadPyodide({
     indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
   });
   await pyodide.loadPackage("astropy")
-  // await pyodide.loadPackage("astropy")
-  return pyodide
-}
+
+  const scriptText = await (await fetch(script)).text();
+  return { pyodide, scriptText }
+});
 
 const runScript = async (pyodide: any, code: string) => {
   await pyodide.runPythonAsync(code)
@@ -29,51 +31,52 @@ const runScript = async (pyodide: any, code: string) => {
   console.log("globals", content)
 
   return content
-}
+};
 
 function App() {
   const pyodideObj = useRef<any>(null);
+  const scriptObj = useRef<any>(null);
   const [loadPyodideOK, setLoadPyodideOK] = useState(false);
   useEffect(() => {
     async function init() {
       if (!loadPyodideOK) {
-        pyodideObj.current = await pyodideInstance();
+        const { pyodide, scriptText } = await pyodideInstance();
+        pyodideObj.current = pyodide;
+        scriptObj.current = scriptText
         setLoadPyodideOK(true)
       } else {
         console.log("no duplicate reload to avoid pyodide error")
       }
     }
     init()
-  }, [loadPyodideOK]);
+  }, []);
 
   const [plotdata, setPlotData] = useState([{}]);
   const [plotlayout, setPlotLayout] = useState(Viewer.defaultProps.layout);
 
+  const getData = useCallback(d4c.wrap(async (file: File) => {
+    let content = await runScript(pyodideObj.current, scriptObj.current);
+    // console.log("content", content)
+    // console.log("header", content.header.toJs())
+    const xdata = content.axisdata(1).toJs()
+    const ydata = content.rawdata.toJs()[0][0]
+
+    setPlotData([{
+      x: xdata,
+      y: ydata,
+      type: 'scatter',
+      mode: 'lines+markers',
+    }])
+
+    const newLayout = plotlayout
+    newLayout.title.text = file.name
+    newLayout.xaxis.title.text = content.header.toJs().get('CUNIT1')
+    newLayout.yaxis.title.text = content.header.toJs().get('BUNIT')
+    setPlotLayout(newLayout)
+  }), []);
+
   const readFile = (file: File) => {
     const reader = new FileReader()
-
-    const getData = async () => {
-      const scriptText = await (await fetch(script)).text();
-      // console.log("script text", scriptText)
-      let content = await runScript(pyodideObj.current, scriptText);
-      // console.log("content", content)
-      // console.log("header", content.header.toJs())
-      const xdata = content.axisdata(1).toJs()
-      const ydata = content.rawdata.toJs()[0][0]
-
-      setPlotData([{
-        x: xdata,
-        y: ydata,
-        type: 'scatter',
-        mode: 'lines+markers',
-      }])
-
-      const newLayout = plotlayout
-      newLayout.title.text = file.name
-      newLayout.xaxis.title.text = content.header.toJs().get('CUNIT1')
-      newLayout.yaxis.title.text = content.header.toJs().get('BUNIT')
-      setPlotLayout(newLayout)
-    }
 
     reader.onabort = () => console.log('file reading was aborted')
     reader.onerror = () => console.log('file reading has failed')
@@ -83,7 +86,7 @@ function App() {
       // window.pyodide.loadPackage(['astropy']).then(() => {
       //   getData();
       // })
-      getData();
+      getData(file);
     }
     reader.readAsArrayBuffer(file)
   }
