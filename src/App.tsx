f3314f7035695911b8c757c4d8d32bc1759b80cf
Chia-Jung Hsu <appolloford@gmail.com>
2021-10-21 16:07:05 +0200

    Fix minor bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
 import { FileInput } from '@blueprintjs/core';
 import Viewer from './components/Viewer';
-import script from './python/fitsreader.py';
+import readerDef from './python/fitsreader.py';
 import './App.css';
 
 declare global { // <- [reference](https://stackoverflow.com/a/56458070/11542903)
@@ -44,48 +44,48 @@ function App() {
       }
     }
     init()
-  }, [loadPyodideOK]);
+  }, []);
 
   const [plotdata, setPlotData] = useState([{}]);
   const [plotlayout, setPlotLayout] = useState(Viewer.defaultProps.layout);
 
+  const getData = async (file: File) => {
+    const script = await (await fetch(readerDef)).text();
+    // console.log("script text", scriptText)
+    let content = await runScript(pyodideObj.current, script);
+    // console.log("content", content)
+    // console.log("header", content.header.toJs())
+    const xdata = content.axisdata(1).toJs()
+    const ydata = content.rawdata.toJs()[0][0]
+
+    setPlotData([{
+      x: xdata,
+      y: ydata,
+      type: 'scatter',
+      mode: 'lines+markers',
+    }])
+
+    const newLayout = plotlayout
+    newLayout.title.text = file.name
+    newLayout.xaxis.title.text = content.header.toJs().get('CUNIT1')
+    newLayout.yaxis.title.text = content.header.toJs().get('BUNIT')
+    setPlotLayout(newLayout)
+  }
+
   const readFile = (file: File) => {
-    const reader = new FileReader()
+    if (file) {
+      console.log("processing file:", file)
+      const reader = new FileReader()
 
-    const getData = async () => {
-      const scriptText = await (await fetch(script)).text();
-      // console.log("script text", scriptText)
-      let content = await runScript(pyodideObj.current, scriptText);
-      // console.log("content", content)
-      // console.log("header", content.header.toJs())
-      const xdata = content.axisdata(1).toJs()
-      const ydata = content.rawdata.toJs()[0][0]
-
-      setPlotData([{
-        x: xdata,
-        y: ydata,
-        type: 'scatter',
-        mode: 'lines+markers',
-      }])
-
-      const newLayout = plotlayout
-      newLayout.title.text = file.name
-      newLayout.xaxis.title.text = content.header.toJs().get('CUNIT1')
-      newLayout.yaxis.title.text = content.header.toJs().get('BUNIT')
-      setPlotLayout(newLayout)
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        window.jsarray = new Float32Array(reader.result as ArrayBuffer);
+        getData(file);
+      }
+      reader.readAsArrayBuffer(file)
     }
-
-    reader.onabort = () => console.log('file reading was aborted')
-    reader.onerror = () => console.log('file reading has failed')
-    reader.onload = () => {
-      // Do whatever you want with the file contents
-      window.jsarray = new Float32Array(reader.result as ArrayBuffer);
-      // window.pyodide.loadPackage(['astropy']).then(() => {
-      //   getData();
-      // })
-      getData();
-    }
-    reader.readAsArrayBuffer(file)
   }
 
   return (
