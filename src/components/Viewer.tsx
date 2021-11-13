ac7a4c3be54b487306a8c559a0b7c7d663539563
Chia-Jung Hsu <appolloford@gmail.com>
2021-11-13 14:01:11 +0100

    Save select points

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import HC_exporting from 'highcharts/modules/exporting';
+import { Button, HTMLTable } from "@blueprintjs/core";
 
 HC_exporting(Highcharts);
 
@@ -9,17 +10,11 @@ const Viewer = (props: any) => {
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
   const [cursorX, setCursorX] = useState(0);
   const [cursorY, setCursorY] = useState(0);
+  const [baselinePoints, setBaselinePoints] = useState([[1, 2], [3, 4]]);
 
-  const handleMouseMove = (event: any) => {
-    // const chart = event.target.series.chart;
-    const chart = chartComponentRef.current && chartComponentRef.current.chart;
-    // console.log(chart);
-    // console.log(event);
-    // console.log(Highcharts);
-    // console.log(chartComponentRef);
-    // console.log(event.target);
-    // console.log(event.target.tooltipPos);
-
+  const getCursorPos = (chart: any, event: any) => {
+    let xvalue = 0.0;
+    let yvalue = 0.0;
     if (chart) {
       const e = chart.pointer.normalize(event);
       const x = e.chartX - chart.plotLeft;
@@ -31,12 +26,40 @@ const Viewer = (props: any) => {
       if (x >= 0 && y >= 0 && x <= chart.chartWidth && y <= chart.chartHeight) {
         // console.log(chart.xAxis[0].toValue(x, true));
         // console.log(chart.yAxis[0].toValue(chart.plotHeight - y, true));
-        setCursorX(chart.xAxis[0].toValue(x, true));
-        setCursorY(chart.yAxis[0].toValue(y, true));
+        // setCursorX(chart.xAxis[0].toValue(x, true));
+        // setCursorY(chart.yAxis[0].toValue(y, true));
+        xvalue = chart.xAxis[0].toValue(x, true);
+        yvalue = chart.yAxis[0].toValue(y, true);
       }
     }
+    return { xvalue, yvalue };
+  }
+
+  const handleMouseMove = (event: any) => {
+    // const chart = event.target.series.chart;
+    const chart = chartComponentRef.current?.chart;
+    // console.log(chart);
+    // console.log(event);
+    // console.log(Highcharts);
+    // console.log(chartComponentRef);
+    // console.log(event.target);
+    // console.log(event.target.tooltipPos);
+
+    const { xvalue, yvalue } = getCursorPos(chart, event);
+    setCursorX(xvalue);
+    setCursorY(yvalue);
+
   };
 
+  const addBaselinePoints = (point: Array<number>) => {
+    setBaselinePoints([...baselinePoints, point]);
+  };
+
+  const handleDoubleClick = (event: any, callback: Function) => {
+    const chart = chartComponentRef.current?.chart;
+    const { xvalue, yvalue } = getCursorPos(chart, event);
+    callback([xvalue, yvalue]);
+  };
 
 
   const fileName = props.fileName;
@@ -74,27 +97,16 @@ const Viewer = (props: any) => {
     const header = dataSource.header.toJs();
 
     const xdataArray = [].slice.call(xdata);
-    // const ydataArray = [].slice.call(ydata);
     const seriesData = xdataArray.map((xi: number, i: number) => {
       return [xi, ydata[i]];
     });
-    // console.log(xdata);
-    // console.log(ydata);
-    // console.log(seriesData);
 
     const plotData = [{
       type: 'line',
-      // data: [1, 2, 3],
       data: seriesData,
-      // data: {
-      //   x: xdata,
-      //   y: ydata,
-      // },
     }];
 
     newOptions.series = plotData;
-
-    // console.log(newOptions);
 
     const xAxis = {
       title: {
@@ -124,14 +136,37 @@ const Viewer = (props: any) => {
         highcharts={Highcharts}
         options={newOptions}
         ref={chartComponentRef}
-        containerProps={{ onMouseMove: handleMouseMove }}
+        containerProps={{
+          onMouseMove: handleMouseMove,
+          onDoubleClick: (event: any) => { handleDoubleClick(event, addBaselinePoints) }
+        }}
         {...props}
       />
       <h4>X: {cursorX} Y: {cursorY}</h4>
+      <div>
+        <HTMLTable striped={true} interactive={true} condensed={true}>
+          <caption>baseline fitting points</caption>
+          <thead>
+            <tr>
+              <th>X coordinate</th>
+              <th>Y coordinate</th>
+              <th><Button icon="trash" text="Clear All" onClick={() => { setBaselinePoints([]) }} /></th>
+            </tr>
+          </thead>
+          <tbody>
+            {baselinePoints.map(item => {
+              return (
+                <tr key={item[0]} onClick={() => { console.log("click table") }}>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                  <td><Button icon="cross" minimal={true} onClick={() => { setBaselinePoints(baselinePoints.filter(ele => ele !== item)) }} /></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </HTMLTable>
+      </div>
     </>
-    // <div>
-    //   <Plot data={plotData} layout={plotLayout} />
-    // </div>
   );
 }
 
@@ -152,9 +187,9 @@ Viewer.defaultProps = {
         tooltip: {
           followPointer: false
         },
-      }
+      },
     },
-  },
+  }
 }
 
 export default Viewer;
